d0bcbdbaac548d34b0fd68e8775405bf0f26760d
Ivan Rahn <ivan.rahn@gmail.com>
2018-09-04 11:17:46 +1000

    first commit
    Co-authored-by:
    Co-authored-by: natasha <natashakhema@hotmail.com>

---
+++ lib/breathe/methods.rb
@@ -0,0 +1,26 @@
+#greeting method
+def greeting
+  puts "Hey awesome to see you!"
+end
+
+#main menu method
+def
+  puts "What would you like to keep an eye on today?"
+  Did you smoke in the last 24 hours?
+    How many cigarettes did you smoke in the last 24 hours? (give range)
+
+  if no...Did you have any cravings to smoke today? 
+    ....How would you rate your mood today.(give range)
+
+
+end
+
+
+
+
+
+
+
+
+
+end
