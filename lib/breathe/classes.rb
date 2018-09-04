574e9f2fab0eb2dc3529dfa8dc13077b59c57443
Ivan Rahn <ivan.rahn@gmail.com>
2018-09-04 17:11:19 +1000

    exceptions
    Co-authored-by: natasha <natashakhema@hotmail.com>

--- lib/breathe/classes.rb
+++ lib/breathe/classes.rb
@@ -3,12 +3,16 @@ class User
   attr_accessor :username, :log
 
   def initialize(username)
-    @username = username
+    @username = first(username)
     #we tried array first but decided to go with hash to have dates
     @log = {}
   end
 
-  def method
-    puts "yo"
+  def first(first)
+    if first == nil or first.size == 0 #or first != ~/(A-z0-9)+/
+      raise ArgumentError.new("Everyone must have a valid name.")
+    end
+    first[0] = first[0].chr.capitalize
+    @first = first
   end
 end
