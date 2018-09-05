02d66b68868478193c39c21ef890a2218ce7a9ac
Ivan Rahn <ivan.rahn@gmail.com>
2018-09-05 14:09:56 +1000

    change exception
    Co-authored-by: natasha <natashakhema@hotmail.com>

--- lib/breathe.rb
+++ lib/breathe.rb
@@ -2,10 +2,15 @@ require_relative "breathe/methods"
 require_relative "breathe/classes"
 # Display greeting
 greeting
-# Create new user or retrieve existing user data
-customer = FileOperations.new(gets.chomp).retrieve_data
-# TODO move rescue from name_check in User class here
-# Run the method to prompt questions for smoking behaviour
-smoking(customer)
-# Save data to file
-FileOperations.new(customer).save_to_file
+begin
+  # Create new user or retrieve existing user data
+  customer = FileOperations.new(gets.chomp).retrieve_data
+  # TODO move rescue from name_check in User class here
+  # Run the method to prompt questions for smoking behaviour
+  smoking(customer)
+  # Save data to file
+  FileOperations.new(customer).save_to_file
+  # rescue the exception and run the program again until we get correct input
+rescue ArgumentError => exception
+  retry
+end
